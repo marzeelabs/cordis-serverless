1a0daadd5e39900dfd62948624e24c8def7d511a
Peter Vanhee <peter.vanhee@gmail.com>
2016-11-22 16:58:43 +0100

    Adding organizations endpoint

--- handler.js
+++ handler.js
@@ -3,7 +3,19 @@
 var cp = require('cordis-parser');
 
 module.exports.getProjects = (event, context, callback) => {
-  cp.parseHorizon2020(function(result) {
+  cp.parseHorizon2020Projects(function(result) {
+    // Slice the results until we introduce proper pagination
+    var sliced = result.slice(0,100);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(sliced),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.getOrganizations = (event, context, callback) => {
+  cp.parseHorizon2020Organizations(function(result) {
     // Slice the results until we introduce proper pagination
     var sliced = result.slice(0,100);
     const response = {
